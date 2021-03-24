fdbfd9254458631984d216678f70ec7d67b3376b
dengpengju <dengpju@163.com>
2021-03-24 09:29:38 +0800

    update

--- go.mod
+++ go.mod
@@ -6,5 +6,4 @@ require (
 	gitee.com/dengpju/higo-parameter v1.0.3
 	github.com/dengpju/higo-logger v1.0.3
 	github.com/dengpju/higo-utils v1.0.1
-	github.com/gin-gonic/gin v1.6.3
 )
--- test/main.go
+++ test/main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/dengpju/higo-throw/throw"
 )
 
 func main()  {
 	//自定义日志输出格式
-	throw.LogHandle = func() {
-		throw.LogFormat = "%s (code: %d)"
-		throw.LogInfo = fmt.Sprintf(throw.LogFormat, throw.LogPayload.Msg, throw.LogPayload.Code)
-	}
+	//throw.LogHandle = func() {
+	//	throw.LogFormat = "%s (code: %d)"
+	//	throw.LogInfo = fmt.Sprintf(throw.LogFormat, throw.LogPayload.Msg, throw.LogPayload.Code)
+	//}
 	throw.Throw(throw.Message("uuujjj333"), throw.Code(1))
 	throw.Throw(throw.Message("uuujjj33344"), throw.Code(4))
 	throw.Throw(throw.Message("uuujjj33355"), throw.Code(5))
