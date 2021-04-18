86fb2e556e2f79137fdebb879b0fe740fba2c41a
dengpengju <dengpju@163.com>
2021-04-18 20:59:53 +0800

    优化

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.14
 require (
 	gitee.com/dengpju/higo-parameter v1.0.3
 	github.com/dengpju/higo-logger v1.0.3
-	github.com/dengpju/higo-utils v1.0.6
+	github.com/dengpju/higo-utils v1.0.21
 )
---
+++ throw/Init.go
@@ -0,0 +1,47 @@
+package throw
+
+import (
+	"fmt"
+	"gitee.com/dengpju/higo-parameter/parameter"
+	"github.com/dengpju/higo-utils/utils"
+	"sync"
+)
+
+const (
+	MESSAGE = "message"
+	CODE    = "code"
+	DATA    = "data"
+)
+
+var (
+	once          sync.Once
+	ThrowInstance IThrowable         //异常实例(可自定义)
+	Handle        parameter.Callable //参数处理函数(可自定义)
+	LogHandle     func()             //日志处理函数(可自定义)
+	MapString     utils.MapString
+	LogPayload    *LogContent
+	LogFormat     = "%s (code: %d) at %s:%d" //日志格式(可自定义)a
+	LogInfo       = ""
+)
+
+func init() {
+	once.Do(func() {
+		ThrowInstance = &Throwable{}
+		LogPayload = &LogContent{}
+		MapString = make(utils.MapString)
+		//初始化参数处理函数
+		Handle = func(p *parameter.Parameter) {
+			if p.Name == MESSAGE {
+				LogPayload.Msg = ErrorToString(p.Value)
+			}
+			if p.Name == CODE {
+				LogPayload.Code = p.Value.(int)
+			}
+			MapString.Put(p.Name, p.Value)
+		}
+		//初始化日志处理函数
+		LogHandle = func() {
+			LogInfo = fmt.Sprintf(LogFormat, LogPayload.Msg, LogPayload.Code, LogPayload.File, LogPayload.Line)
+		}
+	})
+}
